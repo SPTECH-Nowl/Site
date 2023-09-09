1841265a1c91639f5641195a57e58c6f4922f7f8
dantaswilljpgdev <dantaswill748@gmail.com>
2023-09-09 15:07:10 -0300

    Validações de olho ativado e desativado, login e cadastro

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,90 @@
 
+var status = 1;
+function senha(){
+	if(status > 0){
+      document.getElementById('senha_input').type='password';
+      document.getElementById("olho_login").classList.remove("icon")
+      document.getElementById("olho_login").classList.add("icon_hide")
+      status = 0;
+    }else{
+    document.getElementById('senha_input').type='text';
+    document.getElementById("olho_login").classList.remove("icon_hide")
+      document.getElementById("olho_login").classList.add("icon")
+      status = 1;
+    
+    }
+     
+}
+
+
+var status_senha = 1;
+function senha_cadastro(){
+
+	if(status > 0){
+      document.getElementById('senha_input_cadastro').type='password';
+      document.getElementById("olho").classList.remove("icon_senha")
+      document.getElementById("olho").classList.add("icon_senha_desativado")
+      status = 0;
+    }else{
+    document.getElementById('senha_input_cadastro').type='text';
+    document.getElementById("olho").classList.remove("icon_senha_desativado")
+    document.getElementById("olho").classList.add("icon_senha")
+      status = 1;
+    
+    }
+     
+}
+
+
+
+var status_confirmar_senha = 1;
+function confirmar_senha_cadastro(){
+
+	if(status > 0){
+      document.getElementById('conf_senha_input').type='password';
+      document.getElementById("olho_confirmar").classList.remove("icon_confirmar")
+      document.getElementById("olho_confirmar").classList.add("icon_confirmar_desativado")
+      status = 0;
+    }else{
+    document.getElementById('conf_senha_input').type='text';
+    document.getElementById("olho_confirmar").classList.remove("icon_confirmar_desativado")
+    document.getElementById("olho_confirmar").classList.add("icon_confirmar")
+      status = 1;
+    
+    }
+     
+}
+
+const password_cadastro = document.getElementById('senha_input_cadastro');
+const icon_senha_cadastro = document.getElementById('icon_senha');
+
+function showHidesenha(){
+    if(password_cadastro.type === 'senha_input_cadastro'){
+        password.setAttribute('type','text');
+        icon_senha_cadastro.classList.add('hide')
+    }
+    else{
+        password.setAttribute('type', 'password');
+        icon.classList.remove('hide')
+    }
+}
+
+const password_confirmar_cadastro = document.getElementById('conf_senha_input');
+const icon_confirmar_senha = document.getElementById('icon_confirmar');
+
+function showHideconfirmar(){
+    if(password_confirmar_cadastro.type === 'conf_senha_input'){
+        password.setAttribute('type','text');
+        icon_senha_cadastro.classList.add('hide')
+    }
+    else{
+        password.setAttribute('type', 'password');
+        icon.classList.remove('hide')
+    }
+}
+
+
+
 
 
 
